fff298921718aad0acce411bbf072a58de134fbb
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2022-04-13 10:53:42 +0200

    Configure global CSS reset

--- apps/web/src/pages/_app.page.tsx
+++ apps/web/src/pages/_app.page.tsx
@@ -1,9 +1,13 @@
+import { globalCss } from '@stitches/react';
 import type { AppProps } from 'next/app';
 
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { reset } from '../../stitches.config';
 
 type PageProps = {};
+
+reset();
 
 const MyApp = ({ Component, pageProps }: AppProps<PageProps>) => {
   const [queryClient] = useState(() => new QueryClient());
