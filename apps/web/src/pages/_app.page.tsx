4753089edb0819f3efe825c2bd875fe228ab2452
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2022-04-13 14:53:12 +0200

    Remove aliases

--- apps/web/src/pages/_app.page.tsx
+++ apps/web/src/pages/_app.page.tsx
@@ -2,18 +2,22 @@ import type { AppProps } from 'next/app';
 
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { reset } from 'ui';
+import { reset, styled } from 'ui';
 
 type PageProps = {};
 
 reset();
+
+const Main = styled('main', { padding: '$4' });
 
 const MyApp = ({ Component, pageProps }: AppProps<PageProps>) => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Main>
+        <Component {...pageProps} />
+      </Main>
     </QueryClientProvider>
   );
 };
--- apps/web/src/pages/index.page.tsx
+++ apps/web/src/pages/index.page.tsx
@@ -1,18 +1,15 @@
-import { DefaultLayout } from '@/layouts/DefaultLayout';
 import { Stack, Text, Header, Inline, Button } from 'ui';
 
 const Index = () => {
   return (
-    <DefaultLayout>
-      <Stack>
-        <Header>Hello World</Header>
-        <Inline>
-          <Text>start hacking!</Text>
-          <Text>start hacking!</Text>
-        </Inline>
-        <Button>click me</Button>
-      </Stack>
-    </DefaultLayout>
+    <Stack>
+      <Header>Hello World</Header>
+      <Inline>
+        <Text>start hacking!</Text>
+        <Text>start hacking!</Text>
+      </Inline>
+      <Button>click me</Button>
+    </Stack>
   );
 };
 
