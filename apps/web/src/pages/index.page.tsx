8cff4670a325250f6efa5f217144a1825cc2ce2b
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2022-04-13 11:01:14 +0200

    Set up Stack component

--- apps/web/src/pages/index.page.tsx
+++ apps/web/src/pages/index.page.tsx
@@ -1,12 +1,46 @@
 import { DefaultLayout } from '@/layouts/DefaultLayout';
+import { styled } from '../../stitches.config';
+
+const Stack = styled('section', {
+  display: 'flex',
+  flexDirection: 'column',
+  variants: {
+    gap: {
+      '1': {
+        gap: '$1',
+      },
+      '2': {
+        gap: '$2',
+      },
+      '3': {
+        gap: '$3',
+      },
+      '4': {
+        gap: '$4',
+      },
+      '5': {
+        gap: '$5',
+      },
+      '6': {
+        gap: '$6',
+      },
+      '7': {
+        gap: '$7',
+      },
+      '8': {
+        gap: '$8',
+      },
+    },
+  },
+});
 
 const Index = () => {
   return (
     <DefaultLayout>
-      <div>
+      <Stack gap="3">
         <h1>Hello World</h1>
         <p>start hacking!</p>
-      </div>
+      </Stack>
     </DefaultLayout>
   );
 };
