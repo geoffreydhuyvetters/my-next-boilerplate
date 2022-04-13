b6ec7a8bfd0cfbb8ac263b75e053f1488735bc42
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2022-04-13 10:48:20 +0200

    Add spacing tokens

--- apps/web/stitches.config.ts
+++ apps/web/stitches.config.ts
@@ -1,3 +1,16 @@
 import { createStitches } from '@stitches/react';
 
-export const { getCssText, styled, css } = createStitches({});
+export const { getCssText, styled, css } = createStitches({
+  theme: {
+    space: {
+      1: '2px',
+      2: '4px',
+      3: '8px',
+      4: '16px',
+      5: '32px',
+      6: '64px',
+      7: '128px',
+      8: '256px',
+    },
+  },
+});
